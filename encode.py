d491c444f2b484de5d55c3637e7e02f2dc53c725
Michael Taggart <michael.t.taggart@gmail.com>
2022-04-06 17:35:26 -0700

    Add encode script

---
+++ encode.py
@@ -0,0 +1,24 @@
+import sys
+from base64 import b64encode
+
+# Grab command line args
+IN_FILE = sys.argv[1]
+B64_ITERS = int(sys.argv[2])
+OUT_FILE = sys.argv[3]
+
+# Retrieve raw shellcode
+with open(IN_FILE, "rb") as f:
+    shellcode = f.read()
+
+# Hex encode the raw shellcode
+shellcode_hex = ",".join(list(map(hex, shellcode)))
+
+# B64 Encode the shellcode
+encoded_shellcode = shellcode_hex.encode()
+
+for i in range(B64_ITERS):
+    encoded_shellcode = b64encode(encoded_shellcode)
+
+# Write out the file
+with open(OUT_FILE, "wb") as f:
+    f.write(encoded_shellcode)
